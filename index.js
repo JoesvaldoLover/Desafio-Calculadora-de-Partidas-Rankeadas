9d9830e6bdf0445ba3f973ad00a9f1cffadbec99
lucas gomes <theminimare4514@gmail.com>
2024-10-30 10:51:07 -0300

    Resolução do desafio

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-/**
- * TAREFAS:
- * 1. Declarar variáveis de vitórias, perdas, saldo de vitórias e nível
- * 2. Criar funções: 
- * 2.1. calcWinRate(vitorias, perdas)
- * 2.2. getRanking(saldoVitorias)
- * 3. Criar mensagem
- */
+function calcWinRate(wins, loses) {
+    return wins - loses;
+}
+
+function getRanking(winRate) {
+    let rank = "Ferro"
+
+    if (winRate > 10 && winRate <= 20) {
+        rank = "Bronze"
+    } else if (winRate > 20 && winRate <= 50) {
+        rank = "Prata"
+    } else if (winRate > 50 && winRate <= 80) {
+        rank = "Ouro"
+    } else if (winRate > 80 && winRate <= 90) {
+        rank = "Diamante"
+    } else if (winRate > 90 && winRate <= 100) {
+        rank = "Lendário"
+    } else if (winRate > 100) {
+        rank = "Imortal"
+    }
+
+    return rank
+}
+
+let wins = 120
+let loses = 65
+
+let winRate = calcWinRate(wins, loses)
+let rank = getRanking(winRate)
+
+console.log(`O Herói tem saldo de ${winRate} está no nível ${rank}`)
