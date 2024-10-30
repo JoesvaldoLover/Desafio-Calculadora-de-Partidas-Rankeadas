f93eba6f1b16774d5019da05cc0c525802b9057f
lucas gomes <theminimare4514@gmail.com>
2024-10-30 10:35:21 -0300

    index.js adicionado

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * TAREFAS:
+ * 1. Declarar variáveis de vitórias, perdas, saldo de vitórias e nível
+ * 2. Criar funções: 
+ * 2.1. calcWinRate(vitorias, perdas)
+ * 2.2. getRanking(saldoVitorias)
+ * 3. Criar mensagem
+ */
